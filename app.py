f0c83192f287e428a1d6160c63e060676f054cd6
angelic-coder19 <angelyuzya7@gmail.com>
2026-03-15 15:55:12 -0500

    Fix scoring to become more symmetrical by adding bonus score if the attempt number is odd and a penalty if it is not.

--- app.py
+++ app.py
@@ -59,7 +59,9 @@ def update_score(current_score: int, outcome: str, attempt_number: int):
         return current_score - 5
 
     if outcome == "Too Low":
-        return current_score - 5
+        if attempt_number % 2 == 0:
+            return current_score - 5
+        return current_score + 5
 
     return current_score
 
